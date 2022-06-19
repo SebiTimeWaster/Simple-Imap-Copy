e3b6d3381281b6a0aa6a38384a20da375da609c3
TimeWaster <timewaster@example.com>
2022-06-19 20:44:02 +0200

    checkin v0.1

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# simple-imap-copy
+# Simple-Imap-Copy
+
 Copies all contents of one IMAP account into another IMAP account.
+
+-   It copies all folders and emails with the corresponding flags
+-   It does not check if the email is already present on the target server, it will just create a new copy of it
+-   It is not meant to backup you emails, just to migrate them to a new account (For backups use something like [OfflineIMAP](http://www.offlineimap.org))
+
+## Dependencies
+
+[Node.js](https://nodejs.org/en/) (Most modern versions should work)
+
+## Installation
+
+```Bash
+git clone https://github.com/SebiTimeWaster/Simple-Imap-Copy.git
+cd Simple-Imap-Copy
+npm install
+```
+
+## Usage
+
+In the file `Simple-Imap-Copy.js` you need to configure your IMAP accounts, then run:
+
+```Bash
+node Simple-Imap-Copy.js
+```
+
+Remember to subscribe to all copied folders on your target IMAP account, otherwise you won't see them in your email program.
+
+## Problems
+
+If you want to open an issue please set "debug" to true in the file `Simple-Imap-Copy.js` and include the complete console output in your issue (Remove personal information!).
+
+## Changelog
+
+Changelog v0.1:
+
+-   Initial checkin
---
+++ Simple-Imap-Copy.js
@@ -0,0 +1,228 @@
+#! /usr/bin/env node
+
+/**
+ * Simple-Imap-Copy v0.1 by TimeWaster
+ *
+ * Licensed under Creative Commons CC0 1.0 Universal
+ *
+ * Configure your mail servers below.
+ * If your servers use special settings you can configure them following this documentation:
+ * https://github.com/mscdex/node-imap#user-content-connection-instance-methods
+ *
+ * --- ✂ ------ ✂ ------ ✂ ------ ✂ ---
+ */
+
+const sourceAccountConfig = {
+    user: 'user@host.tld',
+    password: 'password',
+    host: 'imap-server.tld',
+    port: 993,
+    tls: true,
+};
+
+const destinationAccountConfig = {
+    user: 'user@host.tld',
+    password: 'password',
+    host: 'imap-server.tld',
+    port: 993,
+    tls: true,
+};
+
+const debug = false;
+
+/**
+ * --- ✂ ------ ✂ ------ ✂ ------ ✂ ---
+ */
+
+const Imap = require('imap');
+const clc = require('cli-color');
+
+const log = (text) => console.log(text);
+const error = (text) => console.log(clc.redBright.bold(text));
+const success = (text) => console.log(clc.greenBright(text));
+const keepAliveConfig = { interval: 1000, forceNoop: true };
+
+sourceAccountConfig.keepalive = keepAliveConfig;
+destinationAccountConfig.keepalive = keepAliveConfig;
+
+const srcServer = new Imap(sourceAccountConfig);
+const destServer = new Imap(destinationAccountConfig);
+
+const throwIt = (err, action) => {
+    srcServer.destroy();
+    destServer.destroy();
+
+    error(`\n${err.message} (${action})`);
+
+    if (debug) throw err;
+
+    process.exit(1);
+};
+
+const closeConnections = () => {
+    log(`\nAll done, closing connections`);
+
+    srcServer.end();
+    destServer.end();
+};
+
+const closeBox = (box, nextCallback) => {
+    destServer.closeBox((err) => {
+        if (err) throwIt(err);
+
+        success(`Destination server: ${box.dest} closed`);
+
+        srcServer.closeBox((err) => {
+            if (err) throwIt(err);
+
+            success(`Source server: ${box.src} closed`);
+
+            nextCallback();
+        });
+    });
+};
+
+const copyBox = (box, nextCallback) => {
+    srcServer.search(['ALL'], (err, results) => {
+        if (err) throwIt(err, 'Source Server: Search Mails');
+
+        if (!results || !results.length) {
+            log('No emails found');
+            closeBox(box, nextCallback);
+            return;
+        }
+
+        let mailCount = results.length;
+
+        log(`Emails to copy: ${mailCount}`);
+
+        const fetch = srcServer.fetch(results, { bodies: '' });
+
+        fetch.on('message', (msg, seqno) => {
+            let email;
+            let attrs;
+
+            msg.on('body', (stream) => {
+                stream.on('data', (chunk) => {
+                    email += chunk.toString('utf8');
+                });
+            });
+
+            msg.once('attributes', (attributes) => {
+                attrs = attributes;
+            });
+
+            msg.once('end', () => {
+                destServer.append(email, { date: new Date(attrs.date), flags: attrs.flags }, (err) => {
+                    if (err) throwIt(err, 'Destination Server: Create Mail');
+
+                    success(`Copied email No. ${seqno}`);
+
+                    if (--mailCount === 0) {
+                        closeBox(box, nextCallback);
+                    }
+                });
+            });
+        });
+
+        fetch.once('error', (err) => throwIt(err, 'Source Server: Fetch Mail'));
+    });
+};
+
+const openBox = (box, nextCallback) => {
+    srcServer.openBox(box.src, true, (err) => {
+        if (err) throwIt(err, 'Source Server: Open Box');
+
+        success(`Source server: ${box.src} opened`);
+
+        destServer.openBox(box.dest, false, (err) => {
+            if (err) throwIt(err, 'Destination Server: Open Box');
+
+            success(`Destination server: ${box.dest} opened`);
+
+            copyBox(box, nextCallback);
+        });
+    });
+};
+
+const boxCallback = (box, nextCallback) => () => {
+    log(`\nCopying ${box.src}`);
+
+    if (box.create) {
+        destServer.addBox(box.dest, (err) => {
+            if (err) throwIt(err, 'Destination Server: Create Box');
+
+            success(`Destination server: ${box.dest} was created`);
+
+            openBox(box, nextCallback);
+        });
+    } else {
+        openBox(box, nextCallback);
+    }
+};
+
+const parseBoxes = (boxes, delimiter, output = [], prefix = '') => {
+    for (const [key, value] of Object.entries(boxes)) {
+        const name = `${prefix}${key}`;
+
+        output.push(name);
+
+        if (value.children) parseBoxes(value.children, delimiter, output, `${name}${delimiter}`);
+    }
+
+    return output;
+};
+
+const loadBoxes = () => {
+    log('\nLoad existing boxes');
+
+    srcServer.getBoxes((err, srcBoxes) => {
+        if (err) throwIt(err, 'Source Server: Load Boxes');
+
+        success('Source server: Boxes loaded');
+
+        destServer.getBoxes((err, destBoxes) => {
+            if (err) throwIt(err, 'Destination Server: Load Boxes');
+
+            success('Destination server: Boxes loaded');
+
+            const srcBoxesParsed = parseBoxes(srcBoxes, srcServer.delimiter);
+            const destBoxesParsed = parseBoxes(destBoxes, destServer.delimiter);
+            const boxes = [];
+
+            srcBoxesParsed.forEach((srcBox) => {
+                const destBox = srcBox.replace(new RegExp(`[\\${srcServer.delimiter}]`, 'g'), destServer.delimiter);
+
+                boxes.push({ src: srcBox, dest: destBox, create: destBoxesParsed.includes(destBox) ? false : true });
+            });
+
+            log('\nStart copying boxes');
+
+            // node-imap does not expose the internal promises, make sure everything is executed in sequence
+            let callbackChain = closeConnections;
+            boxes.reverse().forEach((box) => {
+                callbackChain = boxCallback(box, callbackChain);
+            });
+            callbackChain();
+        });
+    });
+};
+
+const connectServer = (server, name, callback) => {
+    server.once('ready', () => {
+        success(`${name} server: Connected`);
+
+        callback();
+    });
+    server.once('error', (err) => throwIt(err, `${name} Server: Connect`));
+    server.once('end', () => success(`${name} server: Disconnected`));
+    server.connect();
+};
+
+try {
+    log('Connecting to servers:');
+
+    connectServer(srcServer, 'Source', () => connectServer(destServer, 'Destination', loadBoxes));
+} catch (err) {
+    throwIt(err, 'Unknown');
+}
